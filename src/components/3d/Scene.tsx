bd8c4b039f2c516ebff7fe3385d70d514ee5a61b
Ruchinzz <ruchinaudichya09@gmail.com>
2025-11-22 17:26:50 +0530

    feat: Add interactive camera controls (drag/touch to rotate)

--- src/components/3d/Scene.tsx
+++ src/components/3d/Scene.tsx
@@ -2,7 +2,7 @@
 
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { Environment, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import World from "./World";
 
 interface SceneProps {
@@ -15,8 +15,23 @@ export default function Scene({ onNodeClick }: SceneProps) {
             <Canvas shadows camera={{ position: [0, 2, 10], fov: 45 }}>
                 <Suspense fallback={null}>
                     <World onNodeClick={onNodeClick} />
-                    <Environment preset="city" />
                 </Suspense>
+                <OrbitControls
+                    enableZoom={true}
+                    enablePan={false}
+                    minDistance={5}
+                    maxDistance={20}
+                    minPolarAngle={Math.PI / 6}
+                    maxPolarAngle={Math.PI / 2.2}
+                    enableDamping={true}
+                    dampingFactor={0.05}
+                    rotateSpeed={0.5}
+                    zoomSpeed={0.8}
+                    touches={{
+                        ONE: 2, // TOUCH_ROTATE
+                        TWO: 1  // TOUCH_DOLLY_PAN
+                    }}
+                />
             </Canvas>
         </div>
     );
